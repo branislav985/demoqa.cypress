6c33f0e39fd3286328ef155481ff206fb72cd0f0
Branislav Andrejic <branislavandrejic@192.168.5.192>
2023-05-19 00:37:16 +0200

    Test(RadioButtons): Check functionality of radio buttons

---
+++ cypress/elements/radioButton.elements.js
@@ -0,0 +1,7 @@
+export default class RadioButtonElements {
+  RADIO_BUTTON = "#item-2";
+  RADIO_YES_BUTTON = "#yesRadio";
+  RADIO_IMPRESSIVE_BUTTON = "#impressiveRadio";
+  RADIO_NO_BUTTON = "#noRadio";
+  SELECTED_VALUE = "p";
+}
